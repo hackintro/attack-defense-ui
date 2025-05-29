38ee2cf414f9255ace228804b71c31df296e4062
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2025-05-29 12:36:11 +0300

    format code

--- src/pages/Truth.jsx
+++ src/pages/Truth.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef, useState } from 'react';
 import reality from '@/assets/truth.mp4';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Truth({ theme, currentTheme, onDataUpdate }) {
   const videoRef = useRef(null);
@@ -18,9 +18,11 @@ export default function Truth({ theme, currentTheme, onDataUpdate }) {
 
   return (
     <main className="container mx-auto flex-1 px-4 py-6">
-      <h1 className="mb-8 text-center text-2xl font-bold italic text-red-500">Is any of it real?</h1>
+      <h1 className="mb-8 text-center text-2xl font-bold italic text-red-500">
+        Is any of it real?
+      </h1>
 
-      <div className="relative w-full max-w-3xl mx-auto overflow-hidden rounded-lg">
+      <div className="relative mx-auto w-full max-w-3xl overflow-hidden rounded-lg">
         {!isLoaded && (
           <div
             className={`absolute inset-0 flex items-center justify-center ${currentTheme.svgBackground}`}
@@ -33,7 +35,7 @@ export default function Truth({ theme, currentTheme, onDataUpdate }) {
 
         <video
           ref={videoRef}
-          className="w-full h-auto max-h-[70vh]"
+          className="h-auto max-h-[70vh] w-full"
           onLoadedData={handleVideoLoad}
           controls
           preload="metadata"
