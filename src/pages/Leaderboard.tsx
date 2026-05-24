86982e8265b9211b6cd0e614103097a876b3443b
mgiannopoulos24 <79588074+mgiannopoulos24@users.noreply.github.com>
2026-05-24 18:12:30 +0300

    Make confetti infinite

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -66,24 +66,11 @@ export default function Leaderboard({ onDataUpdate }: LeaderboardProps) {
   useEffect(() => {
     if (!teams) return;
 
-    const duration = 15 * 1000;
-    const animationEnd = Date.now() + duration;
     const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-    function randomInRange(min: number, max: number) {
-      return Math.random() * (max - min) + min;
-    }
-
     const interval = setInterval(() => {
-      const timeLeft = animationEnd - Date.now();
-
-      if (timeLeft <= 0) {
-        return clearInterval(interval);
-      }
-
-      const particleCount = 50 * (timeLeft / duration);
-      confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
-      confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+      confetti({ ...defaults, particleCount: 50, origin: { x: Math.random() * 0.3, y: Math.random() - 0.2 } });
+      confetti({ ...defaults, particleCount: 50, origin: { x: 0.7 + Math.random() * 0.3, y: Math.random() - 0.2 } });
     }, 250);
 
     return () => clearInterval(interval);
